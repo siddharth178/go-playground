aabf59eb10122503a7da37ca82d590dbe8a4d6ef
siddharth178 <siddharth.karandikar@gmail.com>
2017-04-06 20:08:53 +0530

    Generic CSV unmarshalling and counting
    Unmarshal and count various types of csvs in generic way.

---
+++ generic-unmarshal.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/gocarina/gocsv"
+)
+
+type Book struct {
+	Name string `csv:"name"`
+	Isbn string `csv:"isbn"`
+}
+
+type User struct {
+	Name string `csv:"name"`
+	Age  int    `csv:"age"`
+}
+
+type Counter interface {
+	// this function should return a function that takes struct as argument
+	getCounterFunc() interface{}
+	getCount() int
+}
+
+type UserCounter struct {
+	cnt int
+}
+
+func (uc *UserCounter) getCounterFunc() interface{} {
+	f := func(u User) {
+		fmt.Println(">>", u.Name, u.Age)
+		uc.cnt++
+	}
+	return f
+}
+func (uc *UserCounter) getCount() int {
+	return uc.cnt
+}
+
+type BookCounter struct {
+	bookSet map[Book]struct{}
+}
+
+func (bc *BookCounter) getCounterFunc() interface{} {
+	bc.bookSet = make(map[Book]struct{})
+	f := func(b Book) {
+		fmt.Println(">>", b.Name, b.Isbn)
+		if _, ok := bc.bookSet[b]; !ok {
+			bc.bookSet[b] = struct{}{}
+		}
+	}
+	return f
+}
+func (bc *BookCounter) getCount() int {
+	return len(bc.bookSet)
+}
+
+func countRecords(r io.Reader, cntr Counter) (int, error) {
+	err := gocsv.UnmarshalToCallback(r, cntr.getCounterFunc())
+	if err != nil {
+		return 0, err
+	}
+	return cntr.getCount(), nil
+}
+
+func main() {
+
+	booksData := `name,isbn
+Book 1,100
+Book 2,101
+Book 3,103
+Book 1,100
+Book 2,101`
+
+	usersData := `name,age
+F1 L1,30
+F2 L2,20
+F3 L3,70`
+
+	cnt, err := countRecords(bytes.NewBufferString(usersData), &UserCounter{})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("User count:", cnt)
+	}
+
+	cnt, err = countRecords(bytes.NewBufferString(booksData), &BookCounter{})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Unique book count:", cnt)
+	}
+
+}
